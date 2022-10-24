72e3405a8acff49f47f46c0143e7e19f0227b75c
程旭东 <xiyang080@126.com>
2022-10-24 17:42:32 +0800

    tailwindcss test

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,62 @@
-import React, { useState } from "react";
-import { Button, Upload } from "antd";
-// import { useAntdTable } from 'ahooks'
+import React, { useState } from 'react'
+import { Button, Table } from 'antd'
+import { useAntdTable } from 'ahooks'
 // import 'antd/es/upload/style/index.less'
 // import 'antd/es/button/style/index.less'
-// import EditModal from './editModal'
-
-interface Item {
-  name: {
-    last: string;
-  };
-  email: string;
-  phone: string;
-  gender: "male" | "female";
-}
-
-interface Result {
-  total: number;
-  list: Item[];
-}
-
-// const getTableData = ({ current, pageSize }): Promise<Result> => {
-//   let query = `page=${current}&size=${pageSize}`
-
-//   return fetch(`https://randomuser.me/api?results=9&${query}`)
-//     .then((res) => res.json())
-//     .then((res) => ({
-//       total: res.info.results,
-//       list: res.results,
-//     }))
-// }
+import EditModal from './editModal'
 
 export default () => {
-  // const { tableProps } = useAntdTable(getTableData)
-  const [visible, setVisible] = useState(false);
-  const [initProps, setInitProps] = useState({});
+  const [visible, setVisible] = useState(false)
+  const [initProps, setInitProps] = useState({})
 
   const edit = (record: any) => {
-    setInitProps(record);
-    setVisible(true);
-  };
+    setInitProps(record)
+    setVisible(true)
+  }
   const handleOk = () => {
-    console.log("okkkkk");
-    setVisible(false);
-  };
-  // console.log('tableProps', tableProps)
+    console.log('okkkkk')
+    setVisible(false)
+  }
   const columns = [
     {
-      title: "name",
-      dataIndex: ["name", "last"],
+      title: 'name',
+      dataIndex: 'name',
     },
     {
-      title: "email",
-      dataIndex: "email",
+      title: 'email',
+      dataIndex: 'email',
     },
     {
-      title: "phone",
-      dataIndex: "phone",
+      title: 'phone',
+      dataIndex: 'phone',
     },
     {
-      title: "gender",
-      dataIndex: "gender",
-    },
-    {
-      title: "handle",
+      title: 'handle',
       render: (_: any, record: any) => (
-        <Button type="primary" onClick={() => edit(record)}>
+        <Button type='primary' onClick={() => edit(record)}>
           编辑
         </Button>
       ),
     },
-  ];
-  const onChange = () => {};
-  const onUploadChange = () => {};
+  ]
+  const data = [
+    {
+      name: 'zs',
+      email: 'laksdjf',
+      phone: '121212',
+    },
+    {
+      name: 'ls',
+      email: 'laksdjf',
+      phone: '121212',
+    },
+  ]
   return (
-    // <div style={{ width: '100%' }}>
-    //   <Table columns={columns} rowKey='email' scroll={{ y: 400 }} {...tableProps} />
-    //   <EditModal visible={visible} initProps={initProps} onOk={handleOk} />
-    // </div>
-    <div className=" h-20 w-8 border border-red-500 ">
-      {/* <input type='file' onChange={onChange} /> */}
-      <Upload
-        name="file upload"
-        onChange={onUploadChange}
-        beforeUpload={() => false}
-      >
-        <Button>playground</Button>
-      </Upload>
+    <div className='w-full'>
+      ``
+      <Button type='primary'>新增</Button>
+      <Table columns={columns} dataSource={data} rowKey='email' scroll={{ y: 400 }} />
+      <EditModal visible={visible} initProps={initProps} onOk={handleOk} />
     </div>
-  );
-};
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        "bg-head": "#e3e4e5",
+        'bg-head': '#e3e4e5',
       },
       screens: {
-        xl: "1200px",
+        xl: '1200px',
       },
     },
   },
   plugins: [],
-};
+  corePlugins: {
+    preflight: false,
+  },
+}
